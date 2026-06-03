ba54dbd89db3fd55bec3ebacd69f0403f64bc25c
Chenqi Meng <mcqgravity@gmail.com>
2026-06-03 22:48:39 +0800

    refactor: extract comment routes to api_comments.py

--- peerpedia/web/routes/api.py
+++ peerpedia/web/routes/api.py
@@ -10,11 +10,13 @@ from peerpedia.web.routes.api_articles import router as articles_router
 from peerpedia.web.routes.api_citations import router as citations_router
 from peerpedia.web.routes.api_collab import router as collab_router
 from peerpedia.web.routes.api_lan import router as lan_router
+from peerpedia.web.routes.api_comments import router as comments_router
 from peerpedia.web.routes.api_users import router as users_router
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(articles_router)
+router.include_router(comments_router)
 router.include_router(users_router)
 router.include_router(collab_router)
 router.include_router(citations_router)
